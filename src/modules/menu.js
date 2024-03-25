f7f2408b16549d9730ef7d68603dcac1c225d1d2
Roman Amb <roman83mars@gmail.com>
2024-03-26 02:34:20 +0300

    начало работы над меню

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,3 +1,21 @@
 export const menu = () => {
-    console.log(1);
+    const menuBtn = document.querySelector('.menu')
+    const menu = document.querySelector('menu')
+    const closeBtn = menu.querySelector('.close-btn')
+
+    menuBtn.addEventListener('click', () => {
+        if (!menu.style.transform) {
+            menu.style.transform = `translateX(0)`
+        } else {
+            menu.style.transform = ``
+        }
+    })
+
+    closeBtn.addEventListener('click', () => {
+        if (!menu.style.transform) {
+            menu.style.transform = `translateX(0)`
+        } else {
+            menu.style.transform = ``
+        }
+    })
 }
